9f03ba66831eb822d9c4a671e160527da11cf81f
FalconPilot <falconpilot@Mc-Hintosh.local>
2015-11-25 18:26:23 +0100

    Improved toupper/lower and updated header

--- libft_project/libft2/libft.h
+++ libft_project/libft2/libft.h
@@ -14,16 +14,19 @@
 # define LIBFT_H
 # include <string.h>
 
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strcnmp(const char *s1, const char *s2, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int			ft_atoi(const char *str);
+void		ft_bzero(void *s, size_t n);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+void		*ft_memset(void *b, int c, size_t len);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strdup(const char *s1);
+size_t		ft_strlen(const char *s);
+int			ft_strcnmp(const char *s1, const char *s2, size_t n);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 
 #endif
