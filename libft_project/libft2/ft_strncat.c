d4cfaeac19f5acd02ece0100f376d7007c29ce56
FalconPilot <falconpilot@Mc-Hintosh.local>
2015-11-25 19:05:22 +0100

    Updated header, makefile and strcat/strncat

--- libft_project/libft2/ft_strncat.c
+++ libft_project/libft2/ft_strncat.c
@@ -2,7 +2,19 @@
 
 #include <libft.h>
 
-char	*ft_strncat(char *s1, const char *s2, size_t n)
+char	*ft_strcat(char *s1, const char *s2, size_t n)
 {
+	int		i;
+	size_t	i2;
+
+	i = ft_strlen(s1);
+	i2 = 0;
+	while (s2[i2] && i2 < n)
+	{
+		s1[i] = s2[i2];
+		i++;
+		i2++;
+	}
+	s1[i] = '\0';
 	return (s1);
 }
