d66b1f4ba04d447ff2d157eabf78481276c8c493
FalconPilot <falconpilot@Mc-Hintosh.local>
2015-11-27 15:53:01 +0100

    corrected strlcat

--- libft_project/libft2/ft_strlcat.c
+++ libft_project/libft2/ft_strlcat.c
@@ -14,14 +14,17 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int		i;
-	size_t	ret;
+	size_t	i;
+	int		i2;
 
 	i = 0;
-	ret = size;
-	while (dst[size - ret] && ret > 0)
-		ret--;
-	if (ret)
-		return (ret);
-	return (ret);
+	while (dst[i])
+		i++;
+	while (i < size && src[i2])
+	{
+		dst[i] = src[i2];
+		i++;
+		i2++;
+	}
+	return (i);
 }
