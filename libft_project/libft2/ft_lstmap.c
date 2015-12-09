a105a1c663f83c07def07bbdf108aaae9d160ab8
Raphael PALLIES <rpallies@e2r7p1.42.fr>
2015-12-09 14:23:04 +0100

    Files are now norm-corrected

--- libft_project/libft2/ft_lstmap.c
+++ libft_project/libft2/ft_lstmap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpallies <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/09 14:19:45 by rpallies          #+#    #+#             */
+/*   Updated: 2015/12/09 14:19:55 by rpallies         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
@@ -6,7 +18,7 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list	*begin;
 	t_list	*tmp;
 	t_list	*cell;
-	
+
 	if (!lst || !f)
 		return (NULL);
 	tmp = f(lst);
